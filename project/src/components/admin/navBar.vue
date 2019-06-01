6b846a2ef681137ddab484836562481765240112
andynoway253 <andynoway253@gmail.com>
2019-06-02 01:26:08 +0800

    '後臺多餘程式刪除'

--- project/src/components/admin/navBar.vue
+++ project/src/components/admin/navBar.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
     nav(class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow")
         a(class="navbar-brand col-sm-3 col-md-2 mr-0" href="#/adminindex") Sweetaste後臺管理
-        input(class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search")
         ul(class="navbar-nav px-3")
             li(class="nav-item text-nowrap")
-                a(class="nav-link" href="/" @click="singout()") Sign out
+                a(class="nav-link" @click="singout()" style="cursor: pointer;")  Sign out
 </template>
 
 <script>
